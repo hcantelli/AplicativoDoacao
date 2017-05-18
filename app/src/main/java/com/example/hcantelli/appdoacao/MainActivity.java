18c724cbdec711e52d0eee537bffea45d217d3fd
Helio <hcantelli@gmail.com>
2017-05-18 00:21:37 -0300

    Version 1

--- app/src/main/java/com/example/hcantelli/appdoacao/MainActivity.java
+++ app/src/main/java/com/example/hcantelli/appdoacao/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState); 
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         btn_cadastro = (Button) findViewById(R.id.btn_cadastro);
