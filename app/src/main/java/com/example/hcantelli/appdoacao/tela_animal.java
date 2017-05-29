95638162631d8fcfe181bf446ff3fbe2520f3b9b
Andre Volpi <devolpi@gmail.com>
2017-05-29 04:03:59 -0300

    Create pet details activity

---
+++ app/src/main/java/com/example/hcantelli/appdoacao/tela_animal.java
@@ -0,0 +1,13 @@
+package com.example.hcantelli.appdoacao;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class tela_animal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_animal);
+    }
+}
