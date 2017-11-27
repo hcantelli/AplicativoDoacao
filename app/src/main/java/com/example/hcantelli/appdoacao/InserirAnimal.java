45bdf4bde73f773c678b8eb0cd33c748e7fb070c
hcantelli <khudshl9>
2017-11-27 00:15:01 -0200

    Version 5.5 - Continuação da Inserção de Animais pt1

--- app/src/main/java/com/example/hcantelli/appdoacao/InserirAnimal.java
+++ app/src/main/java/com/example/hcantelli/appdoacao/InserirAnimal.java
@@ -6,9 +6,11 @@ import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -27,6 +29,10 @@ import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class InserirAnimal extends AppCompatActivity{
 
@@ -59,10 +65,7 @@ public class InserirAnimal extends AppCompatActivity{
             public void onClick(View v) {
 
                 if(InserirAnimal.this.checkSelfPermission(android.Manifest.permission.CAMERA)== PackageManager.PERMISSION_GRANTED){
-                    Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                    if(takePictureIntent.resolveActivity(getPackageManager()) != null){
-                        startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-                    }
+                    dispatchTakePictureIntent();
                 }else{
                     Toast.makeText(InserirAnimal.this,getText(R.string.permissao_camera),Toast.LENGTH_LONG).show();
                     requestPermissions(new String[]{
@@ -81,29 +84,64 @@ public class InserirAnimal extends AppCompatActivity{
         super.onActivityResult(requestCode, resultCode, data);
 
         if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK){
-            Bitmap imageBitmap = (Bitmap) data.getExtras().get("data");
 
-            StorageReference filepath = galeriaImagens_firebase.child(idUsuarioAtributo + ".jpg");
-            fotoAnimal.setImageBitmap(imageBitmap);
-            fotoAnimal.setDrawingCacheEnabled(true);
-            fotoAnimal.buildDrawingCache();
-            Bitmap bitmap = fotoAnimal.getDrawingCache();
-            ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
-            byte[] data1 = baos.toByteArray();
+//            Bitmap imageBitmap = (Bitmap) data.getExtras().get("data");
+//
+//            StorageReference filepath = galeriaImagens_firebase.child(idUsuarioAtributo + ".jpg");
+//            fotoAnimal.setImageBitmap(imageBitmap);
+//            fotoAnimal.setDrawingCacheEnabled(true);
+//            fotoAnimal.buildDrawingCache();
+//            Bitmap bitmap = fotoAnimal.getDrawingCache();
+//            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+//            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+//            byte[] data1 = baos.toByteArray();
+//
+//            UploadTask uploadTask = filepath.putBytes(data1);
+//            uploadTask.addOnFailureListener(new OnFailureListener() {
+//                    @Override
+//                public void onFailure(@NonNull Exception exception) {
+//                }
+//            }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+//                @Override
+//                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+//                    Toast.makeText(InserirAnimal.this, "Deu certo.", Toast.LENGTH_LONG).show();
+//                    Uri downloadUrl = taskSnapshot.getDownloadUrl();
+//                }
+//            });
+        }
+    }
 
-            UploadTask uploadTask = filepath.putBytes(data1);
-            uploadTask.addOnFailureListener(new OnFailureListener() {
-                    @Override
-                public void onFailure(@NonNull Exception exception) {
-                }
-            }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                @Override
-                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                    Toast.makeText(InserirAnimal.this, "Deu certo.", Toast.LENGTH_LONG).show();
-                    Uri downloadUrl = taskSnapshot.getDownloadUrl();
-                }
-            });
+    private File createImageFile() throws Exception{
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+          imageFileName,
+                ".jpg",
+                storageDir
+        );
+        return image;
+    }
+
+    private void dispatchTakePictureIntent(){
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null ){
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            if(photoFile != null){
+                Uri photoUri = FileProvider.getUriForFile(InserirAnimal.this,
+                        "com.example.android.fileprovider",
+                        photoFile
+                );
+                //Verificar erro aqui, photoUri está com valor mas o intent vem nulo
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
         }
     }
 
